abcf1b74abac2b7a9b95f63ed1d4d3d049b1d200
rg <gt.rgchua@gmail.com>
2018-01-26 14:31:58 +0800

    no message

--- src/webapi/routes.ts
+++ src/webapi/routes.ts
@@ -14,6 +14,7 @@ export default function registerRoutes(router: KoaRouter) {
 	router
 		.post("/codeCommitted", (ctx, next) => {
 			console.log(JSON.stringify(ctx));
+			console.log("code push detected!");
 		});
 
 	router.post("/server/", "start", async (ctx, next) => {
